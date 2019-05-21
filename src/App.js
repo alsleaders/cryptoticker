2893a27ac69a080f9866bf5b8ae643ea303b4bc9
Amanda Leaders <alsleaders@gmail.com>
2019-05-21 13:01:03 -0400

    Header and coins set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import Coins from './components/Coins'
+import Headers from './components/Headers'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return <>Headers </>
   }
 }
 
---
+++ src/components/Coins.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Coins extends Component {
+  render() {
+    return (
+      <h1>
+        Lots of Coins! Lots of Coins! Lots of Coins! Lots of Coins! Lots of
+        Coins!
+      </h1>
+    )
+  }
+}
+
+export default Coins
